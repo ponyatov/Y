81e7470d2dc5198da5200f02a69426801e1e48de
Dmitry Ponyatov <dponyatov@gmail.com>
2015-07-30 17:01:30 +0400

    + comment + int
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- h.h
+++ h.h
@@ -2,10 +2,14 @@
 #define _H_H
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 // lexer/parser
 extern int yylex(void);
 extern int yyparse(void);
 extern void yyerror(const char *msg);
+
+#include "parser.tab.hpp"
 
 #endif // _H_H
