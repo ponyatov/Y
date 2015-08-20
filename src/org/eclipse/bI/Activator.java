749e10dad98757dd91e164a182ef346a8ccf2434
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-20 17:24:39 +0400

    + Activator & Perspective srcs
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/org/eclipse/bI/Activator.java
@@ -0,0 +1,7 @@
+package org.eclipse.bI;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class Activator extends AbstractUIPlugin {
+	public static final String PLUGIN_ID = "org.eclipse.bI";
+}
