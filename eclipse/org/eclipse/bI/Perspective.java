9d372efddff1c038fec040a5d1089963953c3bbe
Dmitry Ponyatov <dponyatov@gmail.com>
2015-08-25 15:04:12 +0400

    * src -> eclipse + bI activity filtering unwanted
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- eclipse/org/eclipse/bI/Perspective.java
+++ eclipse/org/eclipse/bI/Perspective.java
@@ -16,6 +16,9 @@ public class Perspective implements IPerspectiveFactory {
 	}
 
 	private void defineActions(IPageLayout layout) {
+		// new menu items
+//		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
+//		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");
 		// views
 		layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
 		layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
