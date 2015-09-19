017477ec88c0bc8d8be470d9dccc66af2308dd92
Dmitry Ponyatov <dponyatov@gmail.com>
2015-09-20 01:34:22 +0400

    -eclipse
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- eclipse/org/eclipse/bI/Activator.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.bI;
-
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-
-public class Activator extends AbstractUIPlugin {
-	public static final String PLUGIN_ID = "org.eclipse.bI";
-}
