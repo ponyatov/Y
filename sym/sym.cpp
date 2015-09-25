50cd8834539689f53db8f88f30ba6dfc32835ff1
Dmitry Ponyatov <dponyatov@gmail.com>
2015-09-25 18:56:30 +0400

    * sym/error + sym/.module
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- sym/sym.cpp
+++ sym/sym.cpp
@@ -1,16 +1,24 @@
 
 #include "sym.hpp"
 
-sym::sym(string C,char* V) { cls=C; val=string(V); }
+sym::sym(string C,string V)	{ cls=C; val=V; }
+sym::sym(sym* A, sym* B)	{ cls=A->val; val=B->val; }
 
 string sym::dump()	{ return "<"+cls+":"+val+">"; }
 string sym::eval()	{ return val; }
 
+module::module(sym* A, sym* B):sym(A,B)		{ mkdir(val.c_str()); }
+module::module(string A):sym(".module",A)	{ mkdir(val.c_str()); }
+
+module curr_module("tmp");
+
 void yyerror(string s) {
-	cerr << s << " # " << yylineno << " : " << yytext << "\n\n";
-	cout << s << " # " << yylineno << " : " << yytext << "\n\n";
+	cerr << "\n\n" << s << " # " << yylineno << " : " << yytext << "\n\n";
+	cout << "\n\n" << s << " # " << yylineno << " : " << yytext << "\n\n";
 	exit(-1);
 }
+
+void W(string s)	{ cout << s; }
 
 int main(int argc, char *argv[]) {
 	cout << "# " << argv[0] << "\n";
--- sym/sym.hpp
+++ sym/sym.hpp
@@ -8,16 +8,27 @@
 #include <list>
 #include <map>
 
+#ifdef __MINGW32__
+#include <direct.h>
+#endif
+
 using namespace std;
 
 struct sym {
 	string cls;				// class
 	string val;				// value
 	list<sym*> nest;		// nested syms list
-	sym(string,char*);		// token constructor
+	sym(string,string);		// token constructor
+	sym(sym*,sym*);			// pairing constructor
 	virtual string dump();	// dump sym internals for debugging
 	virtual string eval();	// evaluate (compute) object
 };
+
+struct module: sym {
+	module(string);
+	module(sym*,sym*);
+};
+extern module curr_module;
 
 extern int yyparse();
 extern void yyerror(string);
@@ -25,6 +36,8 @@ extern int yylex();
 extern int yylineno;
 extern char* yytext;
 #include "sym.tab.hpp"
+
+extern void W(string);
 
 #endif // _H_SYM
 
