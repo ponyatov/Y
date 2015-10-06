8cb7b92e7221698bc7fc49a6c26289dfddaed297
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-06 10:56:56 +0400

    * vimedit .bIog .toc
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- sym/vimedit.bat
+++ sym/vimedit.bat
@@ -1,1 +1,1 @@
-gvim -p sym.bI README.md .gitignore Makefile sym.ypp sym.lpp sym.hpp sym.cpp vimedit.bat
+gvim -p sym.bI sym.bIog sym.toc README.md .gitignore Makefile sym.ypp sym.lpp sym.hpp sym.cpp vimedit.bat
