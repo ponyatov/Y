22573f21374afa149ae481b573e437e43be07938
Dmitry Ponyatov <dponyatov@gmail.com>
2015-10-19 18:49:14 +0400

    *upgrade
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- nuff/sym.cpp
+++ nuff/sym.cpp
@@ -1,5 +1,20 @@
 #include "sym.hpp"
 
+// \\ object
+
+object::object(string T, string V)	{ tag=T; value=V; }
+string object::tagval()				{ return tag+":"+value; }
+string object::dump(int depth)		{ return "\n"+pad(depth)+tagval(); }
+object* object::eval()				{ value = env[value]; return this; }
+
+string object::pad(int n) {
+	string S;
+	for (int i=0 ; i<n ; i++) S+="\t";
+	return S;
+}
+
+// // object
+
 map<string,string> env;
 
 void env_init() {
--- nuff/sym.hpp
+++ nuff/sym.hpp
@@ -4,7 +4,20 @@
 #include <iostream>					// \\ std C++ includes
 #include <cstdlib>
 #include <map>
+#include <vector>
+//@use(iostream)
 using namespace std;				// //
+
+struct object {						// \\ symbolic object
+	object(string,string);			// constructor
+	string tag;						// class tag
+	string value;					// object value in string form
+	vector<object*> nest;			// nested objects
+	string dump(int depth=0);		// dump object in AST text form
+	object* eval();					// compute (evaluate) object with nested
+	string pad(int);				// pad element using tabs
+	string tagval();				// return object header in string repr
+};									// //
 
 extern map<string,string> env;		// \\ vars registry
 extern void env_init();				// //
