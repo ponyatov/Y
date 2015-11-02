82de05199b4765eaa68c503caa3bf75c0a0ec65c
Dmitry Ponyatov <dponyatov@gmail.com>
2015-11-02 08:57:09 +0400

    * vimedit
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- sym/vimedit.bat
+++ sym/vimedit.bat
@@ -1,1 +1,4 @@
-@gvim -p sym.sym sym.log sym.ypp sym.lpp sym.hpp sym.cpp Makefile vimedit.bat 
+@start .
+@start git gui
+@start sym/vimedit.bat
+@start gvim -p sym.sym sym.log sym.ypp sym.lpp sym.hpp sym.cpp Makefile vimedit.bat 
