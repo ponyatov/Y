64a83e0b4bcea1a15c5342c5e2f199a6bd03c036
Dmitry Ponyatov <dponyatov@gmail.com>
2015-11-16 16:10:56 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bat.bat
+++ bat.bat
@@ -1,2 +1,2 @@
 @start .
-@gvim -p pil.pil log.pilog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile filetype.vim syntax.vim
+@gvim -p pil.pil pil.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile filetype.vim syntax.vim
