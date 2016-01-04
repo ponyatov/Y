ffaabd93cee0539f146aa2442c8f4c49239f1427
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-04 23:41:11 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hpp.hpp
+++ hpp.hpp
@@ -1,3 +1,14 @@
 #ifndef _H_bI
 #define _H_bI
+
+														// == lexer interface ==
+extern int yylex();										// parse next token
+extern int yylineno;									// current source line
+extern char* yytext;									// found token text
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+														// == parser interface ==
+extern int yyparse();									// run parser
+extern void yyerror(std::string);						// error callback
+#include "ypp.tab.hpp"									// token defines for lexer
+
 #endif // _H_bI
