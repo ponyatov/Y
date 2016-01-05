87207bb64ecee3dae784600117a7b905a07baa98
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-05 21:48:51 +0400

    * .hpp/.cpp meta constants recovered
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hpp.hpp
+++ hpp.hpp
@@ -1,5 +1,13 @@
 #ifndef _H_bI
 #define _H_bI
+														// == metainfo constants
+
+#define AUTHOR "(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved"
+#define LICENSE "http://www.gnu.org/copyleft/lesser.html"
+#define GITHUB "https://github.com/ponyatov/Y/tree/dev"
+#define AUTOGEN "/***** DO NOT EDIT: this file was autogened by bI *****/"
+#define LOGO "logo64x64"
+#define LISPLOGO "warning64x64"
 														// == std.includes ==
 #include <iostream>
 #include <sstream>
