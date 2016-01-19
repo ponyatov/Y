a4b9fbad97cbda47a560e7083f0489ce7e75514f
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-19 13:15:11 +0400

    + lisp/ (=rework)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lisp/bat.bat
@@ -0,0 +1,2 @@
+@start .
+@gvim -c "colorscheme peachpuff" -p rework.bI rework.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp win32.hpp win32.cpp Makefile bat.bat .gitignore 
--- rework/bat.bat
+++
@@ -1,2 +0,0 @@
-@start .
-@gvim -c "colorscheme peachpuff" -p rework.bI rework.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile bat.bat .gitignore 
