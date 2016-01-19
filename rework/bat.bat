43ca273f2745141016b7f87a8f689fafdf741c64
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-19 12:54:33 +0400

    * make EXE=.elf * .gitignore * bat colorscheme for rework/
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- rework/bat.bat
+++ rework/bat.bat
@@ -1,2 +1,2 @@
 @start .
-@gvim -p rework.bI rework.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile bat.bat .gitignore 
+@gvim -c "colorscheme peachpuff" -p rework.bI rework.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile bat.bat .gitignore 
