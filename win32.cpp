0e7fdb17313eb6ba1350f4e33202b7b88e4bb096
Dmitry Ponyatov <dponyatov@gmail.com>
2016-01-19 19:07:25 +0400

    (-) mingw32.?pp
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- win32.cpp
+++ win32.cpp
@@ -22,7 +22,22 @@ WinClass WinApplication::winclass;					// shared window class
 // ======================================================= shared window class
 
 LRESULT CALLBACK WinClass::WndProc(HWND hwnd,UINT iMsg,WPARAM wp,LPARAM lp) {
-	return DefWindowProc(hwnd,iMsg,wp,lp);
+	switch (iMsg) {
+		case WM_DESTROY: PostQuitMessage(0); return 0;
+		default: return DefWindowProc(hwnd,iMsg,wp,lp);
+	}
+/*
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+		case WM_PAINT:
+			hdc = BeginPaint(hwnd,&ps);
+			GetClientRect(hwnd,&rect);
+			DrawText(hdc,MODULE,-1,&rect,DT_SINGLELINE|DT_CENTER|DT_VCENTER);
+			EndPaint(hwnd,&ps);
+			return 0;
+			return DefWindowProc(hwnd,iMsg,wp,lp);
+*/
 }
 
 WinClass::WinClass() {
@@ -64,6 +79,24 @@ string Message::tagval() { return tagstr(); }
 
 Sym* window(Sym*o) { return new Window(o); }
 
-Window::Window(Sym*o):Sym("window",o->val) {}
+Window::Window(Sym*o):Sym("window",o->val) {
+	assert ( hwnd = CreateWindow(					// == create window ==
+		WinApplication::AppName,					// window class
+		val.c_str(),								// window title
+		WS_OVERLAPPEDWINDOW,						// style
+		CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,//geometry
+		NULL,										// parent win
+		NULL,										// menu
+		WinApplication::hInstance,
+		NULL
+	));
+	WinClass::wins.push_back(&hwnd);				// register for cleanup
+	ShowWindow(hwnd,WinApplication::nCmdShow);		// display window
+	MSG msg;
+	while (GetMessage(&msg,0,0,0)) {				// infty msg dispatcher
+		DispatchMessage(&msg);
+	}
+}
+
 string Window::tagval() { return tagstr(); }
 Window::~Window() { DestroyWindow(hwnd); }
