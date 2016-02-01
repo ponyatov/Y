7bd3c03804e4ef95a5961fec99b54e8e6a671c50
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-01 11:14:18 +0400

    + lex skeleton
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ puresym/hpp.hpp
@@ -0,0 +1,43 @@
+#ifndef _H_PURESYM
+#define _H_PURESYM
+
+										// == std.includes ==
+#include <iostream>
+#include <cstdlib>
+#include <map>
+using namespace std;
+
+struct Sym {							// == Abstract Symbolic Type (AST) ==
+// ---------------------------------------------------------------------------
+	string tag;							// data type / class
+	string val;							// symbol value
+// -------------------------------------------------------------- constructors
+	Sym(string,string);					// <T:V>
+	Sym(string);						// token
+// ------------------------------------------------------------------- dumping
+	string dump(int depth=0);			// dump symbol object as text
+	virtual string tagval();			// <T:V> header string
+// -------------------------------------------------------- compute (evaluate)
+	virtual Sym* eval();
+};
+
+extern void W(Sym*);								// \ ==== writers ====
+extern void W(string);								// /
+
+// ====================================================== GLOBAL ENV{}IRONMENT
+extern map<string,Sym*> env;
+extern void env_init();								// init env{} on startup
+
+// ========================================================== PARSER INTERFACE
+													// == lexer interface ==
+extern int yylex();									// parse next token
+extern int yylineno;								// current source line
+extern char* yytext;								// found token text
+extern void incLude(Sym*inc);						// .include file
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+													// == parser interface ==
+extern int yyparse();								// run parser
+extern void yyerror(string);						// error callback
+#include "ypp.tab.hpp"								// token defines for lexer
+
+#endif // _H_PURESYM
