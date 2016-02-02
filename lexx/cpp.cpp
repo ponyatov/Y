769f2a5b73c670b533c2d8b59c5e83c9226870c8
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-02 12:27:24 +0400

    + lexx/ lex skeleton
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lexx/cpp.cpp
@@ -0,0 +1,25 @@
+#include "hpp.hpp"
+// ======================================================= error callback
+#define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
+void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
+// ======================================================= main()
+int main() { env_init(); return yyparse(); }
+// ======================================================= writers
+void W(Sym* o)		{ cout << o->dump(); }
+void W(string s)	{ cout << s; }
+
+// ============================================== Abstract Symbolic Type (AST)
+
+// ------------------------------------------------------- constructors
+Sym::Sym(string T,string V) { tag=T; val=V; };			// <T:V>
+Sym::Sym(string V):Sym("sym",V)	{}						// token
+
+// ------------------------------------------------------- dumping
+string Sym::tagval() { return "<"+tag+":"+val+">"; }	// <T:V> header string
+string Sym::dump(int depth) { return tagval(); }		// dump as text
+	
+
+// ====================================================== GLOBAL ENV{}IRONMENT
+map<string,Sym*> env;
+void env_init() {									// init env{} on startup
+}
