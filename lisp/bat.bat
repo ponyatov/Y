ea5f4d3a96e4a136d16f5496d011170abc768b24
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-02 14:25:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lisp/bat.bat
+++ lisp/bat.bat
@@ -1,2 +1,2 @@
 @start .
-@gvim -p lexx.bI lexx.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile bat.bat .gitignore README.md 
+@gvim -p lisp.bI lisp.blog ypp.ypp lpp.lpp hpp.hpp cpp.cpp Makefile bat.bat .gitignore README.md 
