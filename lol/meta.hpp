a06195ed32b97cd688186d1def58cb86aae7fdf5
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-05 12:10:30 +0400

    + lol/ : low level LLVM wrapper
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ lol/meta.hpp
@@ -0,0 +1,14 @@
+#ifndef _H_META
+										// ==== metainformation ====
+#define _H_META
+
+#define TITLE "[Lo]w [L]evel Compiler"
+#define ABOUT "LLVM wrapper script"
+#define AUTHOR "(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved"
+#define LICENSE "http://www.gnu.org/copyleft/lesser.html"
+#define GITHUB "https://github.com/ponyatov/Y/tree/dev/lol"
+#define AUTOGEN "/***** DO NOT EDIT: this file was autogened by bI *****/"
+#define LOGO "hedge64x64"
+
+#endif // _H_META
+
