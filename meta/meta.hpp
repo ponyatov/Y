3d7112b765aac57422daa7162643226a4a6f05d9
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-06 17:19:08 +0400

    * meta/ bI model
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ meta/meta.hpp
@@ -0,0 +1,14 @@
+#ifndef _H_META
+										// ==== metainformation ====
+#define _H_META
+
+#define TITLE "metamodel"
+#define ABOUT "bI language system metamodel"
+#define AUTHOR "(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved"
+#define LICENSE "http://www.gnu.org/copyleft/lesser.html"
+#define GITHUB "https://github.com/ponyatov/Y/tree/dev"
+#define AUTOGEN "/***** DO NOT EDIT: this file was autogened by bI *****/"
+#define LOGO "triangle"
+#define COLOR "darkblue"
+
+#endif // _H_META
