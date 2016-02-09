6ef3dba417165b736fe875410744ce2d9251c2d0
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-09 14:17:38 +0400

    + copypasta bI core objects system
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ toyco/hpp.hpp
@@ -0,0 +1,66 @@
+#ifndef _H_TOYCO
+#define _H_TOYCO
+#include "meta.hpp"
+										// == std.includes ==
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct Sym {							// == Abstract Symbolic Type (AST) ==
+// ---------------------------------------------------------------------------
+	string tag;							// data type / class
+	string val;							// symbol value
+// -------------------------------------------------------------- constructors
+	Sym(string,string);					// <T:V>
+	Sym(string);						// token
+// --------------------------------------------------------- nest[]ed elements
+	vector<Sym*> nest;
+	void push(Sym*);
+// ------------------------------------------------------------------- dumping
+	virtual string dump(int depth=0);	// dump symbol object as text
+	virtual string tagval();			// <T:V> header string
+	string tagstr();					// <T:'V'> Str-like header string
+	string pad(int);					// padding with tree decorators
+// -------------------------------------------------------- compute (evaluate)
+	virtual Sym* eval();
+// ----------------------------------------------------------------- operators	
+	virtual Sym* eq(Sym*);				// A = B	assignment
+	virtual Sym* add(Sym*);				// A + B	add
+};
+
+extern void W(Sym*);								// \ ==== writers ====
+extern void W(string);								// /
+
+// =================================================================== SCALARS
+struct Scalar:Sym { Scalar(string,string);			// scalars common class
+	Sym*eval(); };									// block env[] lookup
+
+struct Str:Scalar { Str(string); string tagval(); Sym*add(Sym*); };
+
+struct Int:Scalar { Int(string); };
+struct Num:Scalar { Num(string); };
+
+// =============================================================== FUNCTIONALS
+// =================================================== operator
+struct Op:Sym { Op(string); Sym* eval(); };
+
+// ====================================================== GLOBAL ENV{}IRONMENT
+extern map<string,Sym*> env;
+extern void env_init();								// init env{} on startup
+
+// ========================================================== PARSER INTERFACE
+													// == lexer interface ==
+extern int yylex();									// parse next token
+extern int yylineno;								// current source line
+extern char* yytext;								// found token text
+extern void incLude(Sym*inc);						// .include file
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+													// == parser interface ==
+extern int yyparse();								// run parser
+extern void yyerror(string);						// error callback
+#include "ypp.tab.hpp"								// token defines for lexer
+
+#endif // _H_TOYCO
+
---
+++ toyco/meta.hpp
@@ -0,0 +1,14 @@
+#ifndef _H_META
+													// ==== metainformation ====
+#define _H_META
+
+#define TITLE "Writing Your Own Toy Compiler Using Flex, Bison and LLVM"
+#define ABOUT "http://gnuu.org/2009/09/18/writing-your-own-toy-compiler/"
+#define AUTHOR "(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved"
+#define LICENSE "http://www.gnu.org/copyleft/lesser.html"
+#define GITHUB "https://github.com/ponyatov/Y/tree/dev"
+#define AUTOGEN "/***** DO NOT EDIT: this file was autogened by bI *****/"
+#define LOGO "hedge64x64"
+#define COLOR "pablo"
+
+#endif // _H_META
