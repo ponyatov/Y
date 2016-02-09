ca014f14090132ce301b85b1597ac34a2dd517a4
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-09 17:57:54 +0400

    * genmodule
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- genmodule.bat
+++ genmodule.bat
@@ -1,9 +1,9 @@
-mkdir %1
+mkdir -p %1
 cd %1
 cp ../lex/genmodule.bI ./%1.bI
 cp ../lex/meta.hpp ./
-ls -la
 echo include ../Makefile > Makefile
+ls -la
 mingw32-make EXE=.exe OS=win32
 mv next/* ./
 cd ..
