7d7429eefa586e1fb3bb4e41ccc51eade2a49d3c
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-10 12:09:53 +0400

    * model/
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- model/README.md
+++ model/README.md
@@ -1,1 +1,10 @@
-### metamodel  
+# ![logo](https://github.com/ponyatov/icons/raw/master/warning.png) model : metamodel
+### tiny language metamodel
+
+(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved
+
+license: http://www.gnu.org/copyleft/lesser.html
+
+GitHub: https://github.com/ponyatov/Y/tree/dev/model/
+
+### ![logo](https://github.com/ponyatov/icons/raw/master/hedge64x64.png) powered by bI dymanic language
---
+++ model/meta.hpp
@@ -0,0 +1,9 @@
+// ==== metainformation ====
+#define TITLE "metamodel"
+#define ABOUT "tiny language metamodel"
+#define AUTHOR "(c) Dmitry Ponyatov <dponyatov@gmail.com>, all rights reserved"
+#define LICENSE "http://www.gnu.org/copyleft/lesser.html"
+#define GITHUB "https://github.com/ponyatov/Y/tree/dev/model/"
+#define AUTOGEN "/***** DO NOT EDIT: this file was autogened by bI *****/"
+#define LOGO "warning"
+#define COLOR "peachpuff"
